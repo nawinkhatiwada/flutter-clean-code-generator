0089ee60af98136c261d5efbd7b6ad1ff174a1f3
unknown <nawin.khatiwada@gmail.com>
2024-07-27 15:55:49 +0545

    Added ability to generate folders inside any directory that has a direct or indirect parent directory containing a features folder

--- src/main/kotlin/com/androidbolts/fluttergenerator/fccg/MyProjectGenerator.kt
+++ src/main/kotlin/com/androidbolts/fluttergenerator/fccg/MyProjectGenerator.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.projectRoots.impl.jdkDownloader.RuntimeChooserMessages.showErrorMessage
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
@@ -15,98 +16,117 @@ class GenerateFolderStructureAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
         val dialog = MyInputDialog()
         dialog.show()
-        ApplicationManager.getApplication().runWriteAction {
-            if (dialog.isOK) {
-                val featureName = dialog.featureName
-                generateFolderStructure(event.project, featureName)
+        if (dialog.isOK) {
+            val featureName = dialog.featureName
+            val selectedDir = getSelectedDirectory(event)
+            val project = event.project
+
+            if (project != null) {
+                ApplicationManager.getApplication().runWriteAction {
+                    if (selectedDir != null) {
+                        val projectDir = project.guessProjectDir()
+                        if (projectDir == null) {
+                            showErrorMessage("No directory selected.")
+                        } else {
+                            val libDir = findOrCreateChildDir(projectDir, "lib")
+                            if (libDir != null) {
+                                val featuresDir = findDir(libDir, "features")
+                                if (featuresDir == null) {
+                                    val createdFeatureDir = createDir(libDir, "features")
+                                    generateFolderStructure(createdFeatureDir, featureName, project)
+                                } else {
+                                    if (isSelectedDirInsideFeatureDir(selectedDir)) {
+                                        generateFolderStructure(selectedDir, featureName, project)
+                                    } else {
+                                        showErrorMessage("Selected directory must be inside features directory.")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                showErrorMessage("Project context is not available.")
             }
         }
-
     }
 
-    private fun generateFolderStructure(project: Project?, featureName: String) {
-        if (project != null) {
-            val baseDir = project.guessProjectDir()
-            val libDir = findLibDirectory(baseDir) ?: return
-            val featureDir = libDir.createChildDirectory(null, featureName)
-            val dataDir = featureDir.createChildDirectory(null, "data")
-            dataDir.createChildDirectory(null, "model")
-
-            val packageName = getPackageName(project) ?: return
-
-
-            dataDir.createChildData(null, "${featureName}_repository.dart")
-                .setBinaryContent("abstract class ${featureName.toCamelCase()}Repository {}".toByteArray())
-            dataDir.createChildData(null, "${featureName}_repository_impl.dart")
-//                .setBinaryContent("class ${featureName.toCamelCase()}RepositoryImpl implements ${featureName.toCamelCase()}Repository {}".toByteArray())
-                .setBinaryContent(getRepositoryImplContent(packageName, featureName).toByteArray())
-
-            val localDir = dataDir.createChildDirectory(null, "local")
-            localDir.createChildData(null, "${featureName}_local.dart")
-                .setBinaryContent("abstract class ${featureName.toCamelCase()}Local {}".toByteArray())
-
-
-
-            localDir.createChildData(null, "${featureName}_local_impl.dart")
-//                .setBinaryContent("class ${featureName.toCamelCase()}LocalImpl implements ${featureName.toCamelCase()}Local {}".toByteArray())
-                .setBinaryContent(getLocalContent(packageName, featureName).toByteArray())
-            val remoteDir = dataDir.createChildDirectory(null, "remote")
-            remoteDir.createChildData(null, "${featureName}_remote.dart")
-                .setBinaryContent("abstract class ${featureName.toCamelCase()}Remote {}".toByteArray())
-
-            remoteDir.createChildData(null, "${featureName}_remote_impl.dart")
-//                .setBinaryContent("class ${featureName.toCamelCase()}RemoteImpl implements ${featureName.toCamelCase()}Remote {}".toByteArray())
-                .setBinaryContent(getRemoteContent(packageName, featureName).toByteArray())
-
-            val presentationDir = featureDir.createChildDirectory(null, "presentation")
-            val providersDir = presentationDir.createChildDirectory(null, "providers")
-
-            val stateDir = providersDir.createChildDirectory(null, "state")
-            stateDir.createChildData(null, "${featureName}_notifier.dart")
-                .setBinaryContent("class ${featureName.toCamelCase()}Notifier {}".toByteArray())
-
-            stateDir.createChildData(null, "${featureName}_state.dart")
-                .setBinaryContent("class ${featureName.toCamelCase()}State {}".toByteArray())
-
-            presentationDir.createChildDirectory(null, "screens")
-            presentationDir.createChildDirectory(null, "widgets")
-            providersDir.createChildData(null, "${featureName}_state_provider.dart")
-                .setBinaryContent("class ${featureName.toCamelCase()}StateProvider {}".toByteArray())
-
-            showToastMessage("Generated Successfully!")
+    private fun isSelectedDirInsideFeatureDir(dir: VirtualFile?): Boolean {
+        var currentDir = dir
+        while (currentDir != null) {
+            val featuresDir = currentDir.findChild("features")
+            if (featuresDir != null) {
+                return true
+            }
+            currentDir = currentDir.parent
         }
+        return false
+    }
+
+    private fun findDir(parentDir: VirtualFile?, dirName: String): VirtualFile? {
+        return parentDir?.findChild(dirName)
+    }
+
+    private fun createDir(parentDir: VirtualFile, dirName: String): VirtualFile {
+        return parentDir.createChildDirectory(null, dirName)
+    }
+
+    private fun findOrCreateChildDir(parentDir: VirtualFile, childName: String): VirtualFile? {
+        return parentDir.findChild(childName) ?: parentDir.createChildDirectory(null, childName)
+    }
+
+    private fun getSelectedDirectory(event: AnActionEvent): VirtualFile? {
+        val selectedFiles = event.getData(com.intellij.openapi.actionSystem.CommonDataKeys.VIRTUAL_FILE_ARRAY)
+        return selectedFiles?.firstOrNull()
+    }
+
+    // new implementation
+    private fun generateFolderStructure(baseDir: VirtualFile, featureName: String, project: Project) {
+        val featureDir = baseDir.createChildDirectory(null, featureName)
+        val dataDir = featureDir.createChildDirectory(null, "data")
+        dataDir.createChildDirectory(null, "model")
+
+        val packageName = getPackageName(project) ?: return
+
+        dataDir.createChildData(null, "${featureName}_repository.dart")
+            .setBinaryContent("abstract class ${featureName.toCamelCase()}Repository {}".toByteArray())
+        dataDir.createChildData(null, "${featureName}_repository_impl.dart")
+            .setBinaryContent(getRepositoryImplContent(packageName, featureName).toByteArray())
+
+        val localDir = dataDir.createChildDirectory(null, "local")
+        localDir.createChildData(null, "${featureName}_local.dart")
+            .setBinaryContent("abstract class ${featureName.toCamelCase()}Local {}".toByteArray())
+
+        localDir.createChildData(null, "${featureName}_local_impl.dart")
+            .setBinaryContent(getLocalContent(packageName, featureName).toByteArray())
+
+        val remoteDir = dataDir.createChildDirectory(null, "remote")
+        remoteDir.createChildData(null, "${featureName}_remote.dart")
+            .setBinaryContent("abstract class ${featureName.toCamelCase()}Remote {}".toByteArray())
+
+        remoteDir.createChildData(null, "${featureName}_remote_impl.dart")
+            .setBinaryContent(getRemoteContent(packageName, featureName).toByteArray())
+
+        val presentationDir = featureDir.createChildDirectory(null, "presentation")
+        val providersDir = presentationDir.createChildDirectory(null, "providers")
+
+        val stateDir = providersDir.createChildDirectory(null, "state")
+        stateDir.createChildData(null, "${featureName}_notifier.dart")
+            .setBinaryContent("class ${featureName.toCamelCase()}Notifier {}".toByteArray())
+
+        stateDir.createChildData(null, "${featureName}_state.dart")
+            .setBinaryContent("class ${featureName.toCamelCase()}State {}".toByteArray())
+
+        presentationDir.createChildDirectory(null, "screens")
+        presentationDir.createChildDirectory(null, "widgets")
+        providersDir.createChildData(null, "${featureName}_state_provider.dart")
+            .setBinaryContent("class ${featureName.toCamelCase()}StateProvider {}".toByteArray())
+
+        showToastMessage("Generated Successfully!")
     }
 
     private fun String.toCamelCase(): String {
         return this.split("_").joinToString("") { it.capitalize() }
-    }
-
-    private fun findLibDirectory(baseDir: VirtualFile?): VirtualFile? {
-        // Look for the "lib" directory directly in the base directory
-        val libDir = baseDir?.findChild("lib")
-        return if (libDir != null && libDir.isDirectory) {
-            /*            val featureDir = libDir.findChild("features")
-                        if (featureDir != null && featureDir.isDirectory) {
-                            featureDir
-                        } else {
-                            libDir.createChildDirectory(null, "feature")
-                        }*/
-            findFeatureDir(libDir)
-
-        } else {
-            // If "lib" directory not found, create one
-            val newLibDir = baseDir?.createChildDirectory(null, "lib")
-            findFeatureDir(newLibDir)
-        }
-    }
-
-    private fun findFeatureDir(libDir: VirtualFile?): VirtualFile? {
-        val featureDir = libDir?.findChild("features")
-        return if (featureDir != null && featureDir.isDirectory) {
-            featureDir
-        } else {
-            libDir?.createChildDirectory(null, "features")
-        }
     }
 
     private fun showToastMessage(message: String) {
@@ -115,8 +135,8 @@ class GenerateFolderStructureAction : AnAction() {
         }
     }
 
-    private fun getPackageName(project: Project): String? {
-        val pubspecFile = project.guessProjectDir()?.findChild("pubspec.yaml") ?: return null
+    private fun getPackageName(project: Project?): String? {
+        val pubspecFile = project?.guessProjectDir()?.findChild("pubspec.yaml") ?: return null
         val psiFile = PsiManager.getInstance(project).findFile(pubspecFile) ?: return null
         val virtualFile = psiFile.virtualFile ?: return null
 
